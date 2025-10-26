fed9946567f55cba0db6051e3598c12bb2776443
fenil2144 <shahfenil1998@gmil.com>
2025-10-26 17:56:31 +0530

    Session 7

---
+++ Session 7 - Maven/maven demo/src/main/java/com/gfg/Application.java
@@ -0,0 +1,11 @@
+package com.gfg;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Addition Result: " + MathOperations.add(10, 20));
+        System.out.println("Subtraction Result: " + MathOperations.subtract(10, 20));
+        System.out.println("Multiplication Result: " + MathOperations.multiply(10, 20));
+        System.out.println("Division Result: " + MathOperations.divide(20, 10));
+    }
+}
---
+++ Session 7 - Maven/maven demo/src/test/java/ApplicationTest.java
@@ -0,0 +1,6 @@
+public class ApplicationTest {
+
+    public static void main(String[] args) {
+        System.out.println("Application Test");
+    }
+}
